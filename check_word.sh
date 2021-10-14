999b6e903feb4985868563c6d35d00620191f6fc
DW0RKiN <ogion@centrum.cz>
2021-10-14 09:52:10 +0100

    Add files via upload

--- check_word.sh
+++ check_word.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-printf "include(\`../M4/FIRST.M4')dnl\n$1\n" | m4
+file=`find ./ -name "FIRST.M4" -exec echo {} \;`
+ test "$file" = "" && file=`find ../ -name "FIRST.M4" -exec echo {} \;`
+if test "$file" = "" ; then
+    printf "$0 error: FIRST.M4 not found!\\n"
+    exit 1
+fi
+printf "include(\`$file')dnl\n$1\n" | m4
