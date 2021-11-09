7d341a6645ee1511792ab6d85cdceff275617ee9
DW0RKiN <ogion@centrum.cz>
2021-11-09 02:54:39 +0000

    Add files via upload

--- check_word.sh
+++ check_word.sh
@@ -5,4 +5,16 @@ if test "$file" = "" ; then
     printf "$0 error: FIRST.M4 not found!\\n"
     exit 1
 fi
-echo "include(\`$file')dnl\n${@}" | m4
+echo "include(\`$file') ${@}" | m4 | awk '
+BEGIN {
+    sum_bytes=0; 
+    sum_clock=0} 
+{
+    match($0,/^[^;]+;([ 0-9]+):([0-9]+)/,arr);
+    sum_bytes+= arr[1]; 
+    sum_clock+=arr[2];
+    print $0
+} 
+END {
+    printf "                        [%2i:%i]\n",sum_bytes,sum_clock
+}'
