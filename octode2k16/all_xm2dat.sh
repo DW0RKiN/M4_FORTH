f53bb0d873e643490512f9f875f2395a861c9ba3
DW0RKiN <ogion@centrum.cz>
2023-06-17 14:52:17 +0100

    Add files via upload

---
+++ octode2k16/all_xm2dat.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#check ./xm2octode2k16
+if [ ! -f ./xm2octode2k16 ] ; then
+	printf "Compile xm2octode2k16.cpp\n"
+	g++ ./xm2octode2k16.cpp -o xm2octode2k16
+fi
+if [ ! -f ./xm2octode2k16 ] ; then
+	echo "File ./xm2octode2k16 not found!" >&2
+	exit 2
+fi
+   
+for file in *.xm; do
+	printf "Convert $file\n"
+	./xm2octode2k16 $file
+	error=$?      
+	[ $error != 0 ] && printf "Error: $error\n" >&2 && exit 1
+done
