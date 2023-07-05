f748937b8cbf082311cefeb428967a6d03a92050
DW0RKiN <ogion@centrum.cz>
2023-07-05 09:11:27 +0100

    Add files via upload

--- fth2m4.sh
+++ fth2m4.sh
@@ -4,11 +4,11 @@
 
 [ $# -gt 2 ] && printf "Use: $0 <soubor_s_programem.fth> <-zfloat>\n" >&2 && exit 1
 
-[ ! -s $1 ] && printf "Error: $1 not found!\n" >&2 && exit 2
+[ ! -s "$1" ] && printf "Error: \"$1\" not found!\n" >&2 && exit 2
 
 AWK_SCRIPT="$(dirname "$(readlink -f "$0")")/fth2m4.awk"
 
-[ ! -s $AWK_SCRIPT ] && printf "Error: $AWK_SCRIPT not found!\n" >&2 && exit 2
+[ ! -s "$AWK_SCRIPT" ] && printf "Error: \"$AWK_SCRIPT\" not found!\n" >&2 && exit 2
 
 
-printf "$(cat $1 | awk -v arg="$2" -f $AWK_SCRIPT)"
+cat "$1" | awk -v arg="$2" -f "$AWK_SCRIPT"
