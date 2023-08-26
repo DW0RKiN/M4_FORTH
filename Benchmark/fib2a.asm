83406a08b7786023888d3ebe9baf9a268ee98af8
DW0RKiN <ogion@centrum.cz>
2023-08-27 00:02:24 +0100

    Add files via upload

--- Benchmark/fib2a.asm
+++ Benchmark/fib2a.asm
@@ -1,39 +1,48 @@
-    ORG 32768
+        ORG 32768
     
+    
+       
+
+    
+          
+              
+                 
+
+    
+                
+    
+            
+        
+    
+
 ;   ===  b e g i n  ===
     ld  (Stop+1), SP    ; 4:20      init   storing the original SP value when the "bye" word is used
     ld    L, 0x1A       ; 2:7       init   Upper screen
     call 0x1605         ; 3:17      init   Open channel
-    ld   HL, 35000      ; 3:10      init   Init Return address stack
+    ld   HL, 0x88B8     ; 3:10      init   Return address stack = 35000
     exx                 ; 1:4       init
-    
     call fib2a_bench    ; 3:17      scall
-    
 Stop:                   ;           stop
     ld   SP, 0x0000     ; 3:10      stop   restoring the original SP value when the "bye" word is used
     ld   HL, 0x2758     ; 3:10      stop
     exx                 ; 1:4       stop
     ret                 ; 1:10      stop
-;   =====  e n d  =====   
-
-
-    
+;   =====  e n d  =====
 ;   ---  the beginning of a data stack function  ---
 fib2a_bench:            ;           ( -- )
-        
-    ld   BC, 0          ; 3:10      xdo(1000,0) 101
-xdo101save:             ;           xdo(1000,0) 101
-    ld  (idx101),BC     ; 4:20      xdo(1000,0) 101
-xdo101:                 ;           xdo(1000,0) 101 
-            
-    ld   BC, 0          ; 3:10      xdo(20,0) 102
-xdo102save:             ;           xdo(20,0) 102
-    ld  (idx102),BC     ; 4:20      xdo(20,0) 102
-xdo102:                 ;           xdo(20,0) 102 
-    push DE             ; 1:11      index(102) xi
-    ex   DE, HL         ; 1:4       index(102) xi
-    ld   HL, (idx102)   ; 3:16      index(102) xi   idx always points to a 16-bit index 
-    push DE             ; 1:11
+    ld   BC, 0x0000     ; 3:10      1000 0 do_101(xm)
+do101save:              ;           1000 0 do_101(xm)
+    ld  [idx101],BC     ; 4:20      1000 0 do_101(xm)
+do101:                  ;           1000 0 do_101(xm)
+    ld   BC, 0x0000     ; 3:10      20 0 do_102(xm)
+do102save:              ;           20 0 do_102(xm)
+    ld  [idx102],BC     ; 4:20      20 0 do_102(xm)
+do102:                  ;           20 0 do_102(xm)
+    push DE             ; 1:11      i_102(m)   ( -- i )
+    ex   DE, HL         ; 1:4       i_102(m)
+    ld   HL, [idx102]   ; 3:16      i_102(m)   idx always points to a 16-bit index
+
+    push DE             ; 1:11      ( n1 -- n2 )
     ld    B, L          ; 1:4
     ld    C, H          ; 1:4       CB = counter
     ld   HL, 0x0000     ; 3:10
@@ -47,33 +56,31 @@ xdo102:                 ;           xdo(20,0) 102
     djnz $-2            ; 2:13/8
     dec  C              ; 1:4
     jr  nz, $-5         ; 2:12/7
-    pop  DE             ; 1:10 
+    pop  DE             ; 1:10
     ex   DE, HL         ; 1:4       drop
-    pop  DE             ; 1:10      drop ( a -- ) 
-                        ;[12:45]    xloop 102   variant +1.B: 0 <= index < stop <= 256, run 20x
-idx102 EQU $+1          ;           xloop 102   idx always points to a 16-bit index
-    ld    A, 0          ; 2:7       xloop 102   0.. +1 ..(20), real_stop:0x0014
-    nop                 ; 1:4       xloop 102   hi(index) = 0 = nop -> idx always points to a 16-bit index.
-    inc   A             ; 1:4       xloop 102   index++
-    ld  (idx102),A      ; 3:13      xloop 102
-    xor  0x14           ; 2:7       xloop 102   lo(real_stop)
-    jp   nz, xdo102     ; 3:10      xloop 102   index-stop
-xleave102:              ;           xloop 102
-xexit102:               ;           xloop 102
-        
-                        ;[16:57/58] xloop 101   variant +1.default: step one, run 1000x
-idx101 EQU $+1          ;           xloop 101   idx always points to a 16-bit index
-    ld   BC, 0x0000     ; 3:10      xloop 101   0.. +1 ..(1000), real_stop:0x03E8
-    inc  BC             ; 1:6       xloop 101   index++
-    ld    A, C          ; 1:4       xloop 101
-    xor  0xE8           ; 2:7       xloop 101   lo(real_stop) first (232>3)
-    jp   nz, xdo101save ; 3:10      xloop 101   3x false positive
-    ld    A, B          ; 1:4       xloop 101
-    xor  0x03           ; 2:7       xloop 101   hi(real_stop)
-    jp   nz, xdo101save ; 3:10      xloop 101   232x false positive if he was first
-xleave101:              ;           xloop 101
-xexit101:               ;           xloop 101
-    
+    pop  DE             ; 1:10      drop   ( a -- )
+                        ;[12:45]    loop_102(xm)   variant +1.B: 0 <= index < stop <= 256, run 20x
+idx102 EQU $+1          ;           loop_102(xm)   idx always points to a 16-bit index
+    ld    A, 0          ; 2:7       loop_102(xm)   0.. +1 ..(20), real_stop:0x0014
+    nop                 ; 1:4       loop_102(xm)   hi(index) = 0 = nop -> idx always points to a 16-bit index.
+    inc   A             ; 1:4       loop_102(xm)   index++
+    ld  [idx102],A      ; 3:13      loop_102(xm)
+    xor  0x14           ; 2:7       loop_102(xm)   lo(real_stop)
+    jp   nz, do102      ; 3:10      loop_102(xm)   index-stop
+leave102:               ;           loop_102(xm)
+exit102:                ;           loop_102(xm)
+                        ;[16:57/58] loop_101(xm)   variant +1.default: step one, run 1000x
+idx101 EQU $+1          ;           loop_101(xm)   idx always points to a 16-bit index
+    ld   BC, 0x0000     ; 3:10      loop_101(xm)   0.. +1 ..(1000), real_stop:0x03E8
+    inc  BC             ; 1:6       loop_101(xm)   index++
+    ld    A, C          ; 1:4       loop_101(xm)
+    xor  0xE8           ; 2:7       loop_101(xm)   lo(real_stop) first (232>3)
+    jp   nz, do101save  ; 3:10      loop_101(xm)   3x false positive
+    ld    A, B          ; 1:4       loop_101(xm)
+    xor  0x03           ; 2:7       loop_101(xm)   hi(real_stop)
+    jp   nz, do101save  ; 3:10      loop_101(xm)   232x false positive if he was first
+leave101:               ;           loop_101(xm)
+exit101:                ;           loop_101(xm)
 fib2a_bench_end:
     ret                 ; 1:10      s;
 ;   ---------  end of data stack function  ---------
--- Benchmark/fib2a.m4
+++ Benchmark/fib2a.m4
@@ -4,9 +4,12 @@ include(`../M4/FIRST.M4')dnl
     SCALL(fib2a_bench)
     STOP   
 
-dnl ( n1 -- n2 )
-define({FIB2_ASM},{
-    push DE             ; 1:11
+    SCOLON(fib2a_bench,( -- ))
+        PUSH2(1000,0) DO 
+            PUSH2(20,0) DO 
+                I 
+__ASM({
+    push DE             ; 1:11      ( n1 -- n2 )
     ld    B, L          ; 1:4
     ld    C, H          ; 1:4       CB = counter
     ld   HL, 0x0000     ; 3:10
@@ -20,11 +23,10 @@ define({FIB2_ASM},{
     djnz $-2            ; 2:13/8
     dec  C              ; 1:4
     jr  nz, $-5         ; 2:12/7
-    pop  DE             ; 1:10})dnl
-dnl
-
-    SCOLON(fib2a_bench,( -- ))
-        XDO(1000,0) 
-            XDO(20,0) XI FIB2_ASM DROP XLOOP
-        XLOOP
+    pop  DE             ; 1:10})
+    
+                DROP
+    
+            LOOP
+        LOOP
     SSEMICOLON
